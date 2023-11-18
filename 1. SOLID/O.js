3423ba7dfc4af4236ee7e43c7e6cbc04346e86fb
howardphungdev <phungthinh0946519356@gmail.com>
2023-11-18 15:52:20 +0700

    open close principle

--- 1. SOLID/O.js
+++ 1. SOLID/O.js
@@ -1,21 +1,25 @@
 ////////////////////////////////
 ////////// 1) BAD  /////////////
 ////////////////////////////////
-const questions = [
-  {
-    type: 'boolean',
-    description: 'This article is very useful',
-  },
-  {
-    type: 'text',
-    description: 'Tell me about yourself',
-  },
-  {
-    type: 'multipleChoice',
-    description: 'What is your favorite language',
-    options: ['HTML', 'CSS', 'JS', 'Golang'],
-  },
-];
+// const questions = [
+//   {
+//     type: 'boolean',
+//     description: 'This article is very useful',
+//   },
+//   {
+//     type: 'text',
+//     description: 'Tell me about yourself',
+//   },
+//   {
+//     type: 'multipleChoice',
+//     description: 'What is your favorite language',
+//     options: ['HTML', 'CSS', 'JS', 'Golang'],
+//   },
+//   {
+//     type: 'range',
+//     description: 'Describe your favorite JS feature',
+//   },
+// ];
 
 function printQuestions(questions) {
   questions.forEach((question) => {
@@ -41,3 +45,67 @@ function printQuestions(questions) {
 ////////////////////////////////
 ////////// 2) GOOD  /////////////
 ////////////////////////////////
+
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('1. True');
+    console.log('1. False');
+  }
+}
+
+class MultipleChoiceQuestion {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
+
+  printQuestionChoices() {
+    this.options.forEach((option, idx) => {
+      console.log(`${idx + 1}: ${option}`);
+    });
+  }
+}
+
+class TextQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Answer: ___________________');
+  }
+}
+
+class RangeQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Minimum: ____________');
+    console.log('Maximum: ____________');
+  }
+}
+
+const questions = [
+  new BooleanQuestion('This article is awesome'),
+  new MultipleChoiceQuestion('What is your favorite movie?', [
+    'The walking dead',
+    'spiderman',
+    'avenger',
+  ]),
+  new TextQuestion('Tell me about yourself?'),
+  new RangeQuestion('Your min and max pushup?'),
+];
+
+function printQuestions(questions) {
+  questions.forEach((question, idx) => {
+    console.log(question.description);
+    question.printQuestionChoices();
+    console.log('');
+  });
+}
